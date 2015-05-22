c39ba1f2d1593731d1f0186c010aa1d9953af046
Stu <twincitiesguy@gmail.com>
2015-05-21 19:54:24 -0500

    corrected dbcp references in sample XML configuration files

--- sample/create_status_table.sql
+++ sample/create_status_table.sql
@@ -1,7 +1,7 @@
 create table sync_status
 (
-  source_id varchar(512) primary key not null,
-  target_id varchar(512),
+  source_id varchar(1024) primary key not null,
+  target_id varchar(1024),
   started_at timestamp null,
   completed_at timestamp null,
   verified_at timestamp null,
