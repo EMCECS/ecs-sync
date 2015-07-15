a883ab1d8731fe457389b48944f97d846a54a2d3
Stu <twincitiesguy@gmail.com>
2015-07-15 15:26:35 -0500

    changes from docker testing

--- docker/start-haproxy.sh
+++ docker/start-haproxy.sh
@@ -5,6 +5,7 @@ if [ $? -ne 0 ]; then
   awk '{X++;print "server ds0"X"-s3 "$1":9020 check"}' ecs_nodes >> haproxy/haproxy.cfg.inst
   docker build -t ecs-haproxy haproxy
   docker run -d --name running-haproxy ecs-haproxy
+  rm haproxy/haproxy.cfg.inst
 else
   docker start running-haproxy
 fi
